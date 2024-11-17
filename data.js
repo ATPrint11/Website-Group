d582229ac55b101a5f274655aca8cf8ee89dfbfa
ATPrint11 <145348386+ATPrint11@users.noreply.github.com>
2024-11-17 19:08:19 +0700

    Add files via upload

---
+++ data.js
@@ -0,0 +1,26 @@
+function updateClock() {
+    const now = new Date();
+    const jam = String(now.getHours()).padStart(2, '0');
+    const menit = String(now.getMinutes()).padStart(2, '0');
+    const detik = String(now.getSeconds()).padStart(2, '0');
+
+    document.getElementById('jam').textContent = jam;
+    document.getElementById('menit').textContent = menit;
+    document.getElementById('detik').textContent = detik;
+}
+
+setInterval(updateClock, 1000);
+
+updateClock();
+
+const signUpButton = document.getElementById('signUp');
+const signInButton = document.getElementById('signIn');
+const container = document.getElementById('container');
+
+signUpButton.addEventListener('click', () => {
+    container.classList.add("right-panel-active");
+});
+
+signInButton.addEventListener('click', () => {
+    container.classList.remove("right-panel-active");
+});
